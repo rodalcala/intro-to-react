4ee7344fbbd845b52df95d212227d8f01662791a
Rodrigo Alcala <alcala@myego2go.de>
2020-06-25 22:37:17 +0200

    chore(RemainingItems): implement useReducer to manage the state

--- src/components/RemainingItems.js
+++ src/components/RemainingItems.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import ItemsList from './ItemsList'
 import Input from './Input'
 
+const initialState = [];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return [...state, action.item];
+    case 'COMPLETE_ITEM':
+      const newState = [...state];
+      newState.splice(action.itemIndex, 1);
+      return newState
+    default:
+      throw new Error();
+  }
+}
+
 function RemainingItems() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function addNewItem(item) {
+    dispatch({ type: 'ADD_ITEM' , item });
+  }
+
+  function removeItem(itemIndex) {
+    dispatch({ type: 'COMPLETE_ITEM' , itemIndex });
+  }
+
   return (
     <main>
-      <ItemsList />
-      <Input />
+      <ItemsList items={state} removeItem={removeItem} />
+      <Input addNewItem={addNewItem} />
     </main>
   );
 }
