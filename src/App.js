cfa29fa314bd6e6ff91f7dd2706de57425f300be
Rodrigo Alcala <rodrigo.alcala.externo@axa-assistance.es>
2020-07-02 15:04:30 +0200

    chore: lift the state 1 level

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
 import RemainingList from './components/RemainingItems';
 
+const initialState = [];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return [...state, action.item];
+    case 'COMPLETE_ITEM':
+      return state.filter((item) => !(item === action.completedItem));
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function addNewItem(item) {
+    dispatch({ type: 'ADD_ITEM' , item });
+  }
+
+  function removeItem(completedItem) {
+    dispatch({ type: 'COMPLETE_ITEM' , completedItem });
+  }
+
   return (
     <div>
       <header>
@@ -11,7 +34,7 @@ function App() {
           to-do
         </h1>
       </header>
-      <RemainingList />
+      <RemainingList state={state} addNewItem={addNewItem} removeItem={removeItem} />
     </div>
   );
 }
--- src/components/RemainingItems.js
+++ src/components/RemainingItems.js
@@ -1,32 +1,9 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 
 import ItemsList from './ItemsList'
 import Input from './Input'
 
-const initialState = [];
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'ADD_ITEM':
-      return [...state, action.item];
-    case 'COMPLETE_ITEM':
-      return state.filter((item) => !(item === action.completedItem));
-    default:
-      return state;
-  }
-}
-
-function RemainingItems() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  function addNewItem(item) {
-    dispatch({ type: 'ADD_ITEM' , item });
-  }
-
-  function removeItem(completedItem) {
-    dispatch({ type: 'COMPLETE_ITEM' , completedItem });
-  }
-
+function RemainingItems({ state, addNewItem, removeItem }) {
   return (
     <main>
       <ItemsList items={state} removeItem={removeItem} />
